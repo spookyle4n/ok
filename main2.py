fd90508433a8daba3f70dcee3bda12323adb2573
spookyle4n <103913003+spookyle4n@users.noreply.github.com>
2022-10-25 14:42:53 -0400

    Update main2.py

--- main2.py
+++ main2.py
@@ -3,15 +3,15 @@ from random import randint
 import time
 import random
 import string
+for i in range(1000000):
+    randomnumber = chr(random.randint(ord('0'), ord('9')))
+    randomnumber2 = chr(random.randint(ord('0'), ord('9')))
+    randomnumber3 = chr(random.randint(ord('0'), ord('9')))
+    randomnumber4 = chr(random.randint(ord('0'), ord('9')))
+    randomnumber5 = chr(random.randint(ord('0'), ord('9')))
+    randomnumber6 = chr(random.randint(ord('0'), ord('9')))
+    randomUpperLetter = chr(random.randint(ord('A'), ord('Z')))
+    randomLowerLetter = chr(random.randint(ord('a'), ord('z')))
 
-randomnumber = chr(random.randint(ord('0'), ord('9')))
-randomnumber2 = chr(random.randint(ord('0'), ord('9')))
-randomnumber3 = chr(random.randint(ord('0'), ord('9')))
-randomnumber4 = chr(random.randint(ord('0'), ord('9')))
-randomnumber5 = chr(random.randint(ord('0'), ord('9')))
-randomnumber6 = chr(random.randint(ord('0'), ord('9')))
-randomUpperLetter = chr(random.randint(ord('A'), ord('Z')))
-randomLowerLetter = chr(random.randint(ord('a'), ord('z')))
-
-for i in range(10):
     print(randomnumber + randomnumber2 + randomnumber3 + randomnumber4 + randomnumber5 + randomnumber6 + randomUpperLetter + randomLowerLetter)
+

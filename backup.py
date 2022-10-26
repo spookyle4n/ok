936453948492732aceaa7ec0ca84d21c3dc51bf1
spookyle4n <103913003+spookyle4n@users.noreply.github.com>
2022-10-26 10:29:18 -0400

    Update backup.py

--- backup.py
+++ backup.py
@@ -21,14 +21,14 @@ print('''
 hi = input("[?] Generate wordlist? [y/n]: ")
 if hi == 'y':
     for i in range(1000000):
-        randomnumber = chr(random.randint(ord('0'), ord('9')))
-    randomnumber2 = chr(random.randint(ord('0'), ord('9')))
-    randomnumber3 = chr(random.randint(ord('0'), ord('9')))
-    randomnumber4 = chr(random.randint(ord('0'), ord('9')))
-    randomnumber5 = chr(random.randint(ord('0'), ord('9')))
-    randomnumber6 = chr(random.randint(ord('0'), ord('9')))
-    randomUpperLetter = chr(random.randint(ord('A'), ord('Z')))
-    randomLowerLetter = chr(random.randint(ord('a'), ord('z')))
+         randomnumber = chr(random.randint(ord('0'), ord('9'))
+         randomnumber2 = chr(random.randint(ord('0'), ord('9')))
+         randomnumber3 = chr(random.randint(ord('0'), ord('9')))
+         randomnumber4 = chr(random.randint(ord('0'), ord('9')))
+         randomnumber5 = chr(random.randint(ord('0'), ord('9')))
+         randomnumber6 = chr(random.randint(ord('0'), ord('9')))
+         randomUpperLetter = chr(random.randint(ord('A'), ord('Z')))
+         randomLowerLetter = chr(random.randint(ord('a'), ord('z')))
 
 print(randomnumber + randomnumber2 + randomnumber3 + randomnumber4 + randomnumber5 + randomnumber6 + randomUpperLetter + randomLowerLetter, flush=True, end= ' ')
 time.sleep(0.00001)

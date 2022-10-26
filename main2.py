23b3ca7bf3b53b3e3a55aa6536427daaa2b57e50
spookyle4n <103913003+spookyle4n@users.noreply.github.com>
2022-10-26 09:50:08 -0400

    Update main2.py

--- main2.py
+++ main2.py
@@ -13,5 +13,5 @@ for i in range(1000000):
     randomUpperLetter = chr(random.randint(ord('A'), ord('Z')))
     randomLowerLetter = chr(random.randint(ord('a'), ord('z')))
 
-    print(randomnumber + randomnumber2 + randomnumber3 + randomnumber4 + randomnumber5 + randomnumber6 + randomUpperLetter + randomLowerLetter)
-
+    print(randomnumber + randomnumber2 + randomnumber3 + randomnumber4 + randomnumber5 + randomnumber6 + randomUpperLetter + randomLowerLetter, flush=True)
+    time.sleep(0.00001)

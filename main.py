d9007e770b4917d9c82e858704d7a045dd25786a
spookyle4n <103913003+spookyle4n@users.noreply.github.com>
2022-10-26 14:43:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,31 @@ from random import randint
 import time
 import random
 import string
-for i in range(1000000):
-    randomnumber = chr(random.randint(ord('0'), ord('9')))
-    randomnumber2 = chr(random.randint(ord('0'), ord('9')))
-    randomnumber3 = chr(random.randint(ord('0'), ord('9')))
-    randomnumber4 = chr(random.randint(ord('0'), ord('9')))
-    randomnumber5 = chr(random.randint(ord('0'), ord('9')))
-    randomnumber6 = chr(random.randint(ord('0'), ord('9')))
-    randomUpperLetter = chr(random.randint(ord('A'), ord('Z')))
-    randomLowerLetter = chr(random.randint(ord('a'), ord('z')))
+print('''
+   ____      ____
+  |    |    |    |_______________________________
+  |    |    |    |         |           |         \
+  |    |    |    |   __    |     __    |    ___   \
+  |    | /\ |    |  |  |   |    |  \___|   |   |  |
+  |    |/  \|    |  |__|   |    |     |    |___|  |
+  \             /          |    |     |           /
+   \___________/___________|____|     |__________/
+   
+               tiktok jahsehrare
+               insta: spookyle4n
 
-    print(randomnumber + randomnumber2 + randomnumber3 + randomnumber4 + randomnumber5 + randomnumber6 + randomUpperLetter + randomLowerLetter, flush=True)
-    time.sleep(0.00001)
+''')
+a = input("[?] Generate wordlist? [y/n]: ")
+if a == 'y':
+    for i in range(10000000):
+        randomnumber = chr(random.randint(ord('0'), ord('9')))
+        randomnumber2 = chr(random.randint(ord('0'), ord('9')))
+        randomnumber3 = chr(random.randint(ord('0'), ord('9')))
+        randomnumber4 = chr(random.randint(ord('0'), ord('9')))
+        randomnumber5 = chr(random.randint(ord('0'), ord('9')))
+        randomnumber6 = chr(random.randint(ord('0'), ord('9')))
+        randomUpperLetter = chr(random.randint(ord('A'), ord('Z')))
+        randomLowerLetter = chr(random.randint(ord('a'), ord('z')))
+
+        print(randomnumber + randomnumber2 + randomnumber3 + randomnumber4 + randomnumber5 + randomnumber6 + randomUpperLetter + randomLowerLetter, flush=True)
+        time.sleep(0.00001)
